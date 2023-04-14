6344730c4baa395100c406f69377b41651b6af9c
Gavin Monahan <gmonahan@setonix-03.pawsey.org.au>
2023-04-14 11:49:46 +0800

    initial commit

---
+++ run_DRAGEN.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l 
+#SBATCH --job-name=DRAGEN_test
+#SBATCH --account=pawsey0848
+#SBATCH --partition=debug
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --nodes=1
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=gavin.monahan@pawsey.org.au
+#SBATCH --mail-type=END
+#SBATCH --error=%j.%x.err
+#SBATCH --output=%j.%x.out
+
+module load gatk4/4.2.5.0--hdfd78af_0
+module load samtools/1.15--h3843a85_0
+
+conda activate gatk
+#Contains Picard, Dragmap
+
+#Run the pipeline
+java -Dconfig.file=/software/projects/pawsey0848/gatk/slurm.conf -jar /software/projects/pawsey0848/gatk/cromwell-85.jar run /software/projects/pawsey0848/gatk/WholeGenomeGermlineSingleSample.wdl \
+   -i /software/projects/pawsey0848/gatk/inputs.json \
+   -o /software/projects/pawsey0848/gatk/cromwell.options
